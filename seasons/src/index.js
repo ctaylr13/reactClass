b55914a01ce67b4791ad873b9432af2d0cbedf5e
ct <ctaylr13@gmail.com>
2019-10-21 19:18:32 -0400

    done

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -16,8 +16,8 @@ class App extends React.Component {
             err => this.setState({ errorMessage: err.message})
         );
     }
-    // example of a class based component 
-    render () {
+
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
@@ -26,7 +26,15 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat}/>;
         }
 
-        return <Spinner/>; 
+        return <Spinner message="please accept the location request"/>; 
+    }
+    // example of a class based component 
+    render () {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
